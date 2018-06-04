4057348f8e08ae8c111830cec1866ff401bb4b96
Ben Coburn <ben@roonlabs.com>
2018-06-04 13:33:22 -0400

    add auto-advance function with configurable interval

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var RoonApi          = require("node-roon-api"),
 var _core      = undefined;
 var _transport = undefined;
 var _output_id = "";
+var _next      = false;
+var _interval  = 5000;
+var _timer     = null;
 
 var roon = new RoonApi({
     extension_id:        'com.roonlabs.looptest',
@@ -24,6 +27,7 @@ var roon = new RoonApi({
                     core.display_version,
                     "-",
                     "PAIRED");
+        
         _transport.subscribe_zones(function(cmd, data) {
             if (cmd == "Changed") {
                 if (data.zones_changed) {
@@ -34,6 +38,15 @@ var roon = new RoonApi({
                                 _transport.control(_output_id, "play");
                                 _transport.mute(_output_id, "mute");
                                 _transport.change_volume(_output_id, "absolute", output.volume.min);
+                            } else {
+                                if ((_timer == null) &&
+                                    ((_next == "\"next\"") || (_next == "next"))) {
+                                    _timer = setInterval(() => {
+                                        if (_transport) {
+                                            _transport.control(_output_id, "next");
+                                        }
+                                    }, _interval);
+                                }
                             }
                         });
                     });
@@ -41,6 +54,7 @@ var roon = new RoonApi({
             }
             
         });
+        
     },
 
     core_unpaired: function(core) {
@@ -59,7 +73,19 @@ var _layout = [
     {
         "type": "zone",
         "title": "Roon output number 1",
-        "setting": "0"
+        "setting": "outputid"
+    },
+    {
+        "type": "string",
+        "title": "type \"next\" to auto-press next button",
+        "setting": "next"
+    },
+    {
+        "type": "integer",
+        "title": "next interval, in MS",
+        "min": 100,
+        "max": 20000,
+        "setting": "interval"
     }
 ];
 
@@ -73,6 +99,8 @@ var svc_settings = new RoonApiSettings(roon, {
     },
     save_settings: function(req, isdryrun, ignored) {
         console.log(req);
+        clearInterval(_timer);
+        _timer = null;
         if (req.body.settings) {
             if (req.body.settings.values) {
                 _values = req.body.settings.values;
@@ -80,8 +108,14 @@ var svc_settings = new RoonApiSettings(roon, {
             if (req.body.settings.layout) {
                 _layout = req.body.settings.layout;
             }
-            if (_values["0"]) {
-                _output_id = _values["0"]["output_id"];
+            if (_values["outputid"]) {
+                _output_id = _values["outputid"]["output_id"];
+            }
+            if (_values["interval"]) {
+                _interval = _values["interval"];
+            }
+            if (_values["next"] != undefined) {
+                _next = _values["next"];
             }
         }
         req.send_complete("Success", { "settings" :{
