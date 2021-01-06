3ef9e8481a23afb689724d521c1be29860a96f0d
Ben Coburn <ben@roonlabs.com>
2021-01-06 11:02:35 -0500

    top results testing tool

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 "use strict";
 
 var RoonApi          = require("node-roon-api"),
-    RoonApiTransport = require('node-roon-api-transport'),
-    RoonApiSettings  = require('node-roon-api-settings');
+    //RoonApiTransport = require('node-roon-api-transport'),
+    RoonApiBrowse    = require('node-roon-api-browse'),
+    RoonApiSettings  = require('node-roon-api-settings'),
+    fs               = require('fs');
 
-var _core      = undefined;
-var _transport = undefined;
-var _output_id = "";
-var _mode      = false;
-var _interval  = 5000;
-var _timer     = null;
+const parse = require('csv-parse');
+const stringify = require('csv-stringify');
+
+var _core       = undefined;
+var _browse     = undefined;
+var _output_id  = "";
+var _search_str = "";
+var _csv_path   = "/home/ben/roonbenchmark/top_bench1.csv";
+var _output_path = "/home/ben/roonbenchmark/top_out1.csv";
+var _view       = {};
+var _next_actions = [];
+var _parsed_lines = [];
+var _stringifier = stringify({ delimiter: ',' });
+var _output_data = [];
+var _desired_id = "";
+var _cur_line = [];
 
 var roon = new RoonApi({
-    extension_id:        'com.roonlabs.looptest',
-    display_name:        'Loop test',
+    extension_id:        'com.roonlabs.searchtest',
+    display_name:        'Search test',
     display_version:     '0.1.0',
     publisher:           'Ben Coburn',
     email:               'ben@roonlabs.com',
@@ -21,51 +33,16 @@ var roon = new RoonApi({
 
     core_paired: function(core) {
         _core      = core;
-        _transport = _core.services.RoonApiTransport;
+        _browse    = _core.services.RoonApiBrowse;
         console.log(core.core_id,
                     core.display_name,
                     core.display_version,
                     "-",
                     "PAIRED");
-        
-        _transport.subscribe_zones(function(cmd, data) {
-            if (cmd == "Changed") {
-                if (data.zones_changed) {
-                    data.zones_changed.forEach(zone => {
-                        zone.outputs.forEach(output => {
-                            if (output.output_id == _output_id) {
-                                if ((_mode == "\"skip\"") || (_mode == "skip")) {
-                                    if (_timer == null) {
-                                        _timer = setInterval(() => {
-                                            if (_transport) {
-                                                _transport.control(_output_id, "next");
-                                            }
-                                        }, _interval);
-                                    }
-                                } else if ((_mode == "\"seek\"") || (_mode == "seek")) {
-                                    console.log(zone.now_playing);
-                                    console.log(zone.now_playing.seek_position !== undefined);
-                                    if (((zone.now_playing != undefined) && (zone.now_playing.seek_position !== undefined) && (zone.now_playing.length != undefined)) &&
-                                        ((zone.now_playing.seek_position == null) ||
-                                         ((zone.now_playing.length - zone.now_playing.seek_position) > 1))) {
-                                        console.log("seek mode triggered");
-                                        _transport.seek(_output_id, "absolute", zone.now_playing.length - 1);
-                                    }
-                                }
-                            }
-
-                            });
-                    });
-                }
-            }
-            
-        });
-        
     },
 
     core_unpaired: function(core) {
         _core      = undefined;
-        _transport = undefined;
         console.log(core.core_id,
                     core.display_name,
                     core.display_version,
@@ -74,25 +51,31 @@ var roon = new RoonApi({
     }
 });
 
-var _values = { };
+var _values = {
+    "csv_path": _csv_path,
+    "output_path": _output_path
+};
 var _layout = [
     {
         "type": "zone",
-        "title": "Roon output number 1",
+        "title": "Roon output",
         "setting": "outputid"
     },
     {
         "type": "string",
-        "title": "test mode",
-        "setting": "mode"
+        "title": "search string",
+        "setting": "search_str"
     },
     {
-        "type": "integer",
-        "title": "next interval, in MS",
-        "min": 100,
-        "max": 20000,
-        "setting": "interval"
-    }
+        "type": "string",
+        "title": "csv path",
+        "setting": "csv_path"
+    },
+    {
+        "type": "string",
+        "title": "output path",
+        "setting": "output_path"
+    } 
 ];
 
 
@@ -105,8 +88,6 @@ var svc_settings = new RoonApiSettings(roon, {
     },
     save_settings: function(req, isdryrun, ignored) {
         console.log(req);
-        clearInterval(_timer);
-        _timer = null;
         if (req.body.settings) {
             if (req.body.settings.values) {
                 _values = req.body.settings.values;
@@ -117,22 +98,170 @@ var svc_settings = new RoonApiSettings(roon, {
             if (_values["outputid"]) {
                 _output_id = _values["outputid"]["output_id"];
             }
-            if (_values["interval"]) {
-                _interval = _values["interval"];
+            if (_values["search_str"] != undefined) {
+                _search_str = _values["search_str"];
             }
-            if (_values["mode"] != undefined) {
-                _mode = _values["mode"];
+            if (_values["csv_path"] != undefined) {
+                _csv_path = _values["csv_path"];
+            }
+            if (_values["output_path"] != undefined) {
+                _output_path = _values["output_path"];
             }
         }
         req.send_complete("Success", { "settings" :{
             "values" : _values,
             "layout" : _layout
         }});
+        if (!isdryrun) {
+            if (_search_str != "") {
+                _next_actions = [0, 0, 0];
+                refresh_browse({ pop_all: true });
+            }
+            if (_csv_path != "") {
+                load_csv();
+            }
+        }
     }
 });
 
+function load_csv() {
+    _stringifier = stringify({ delimiter: ',' });
+    _output_data = [];
+    _stringifier.on('readable', function(){
+        let row;
+        while(row = _stringifier.read()){
+            _output_data.push(row)
+        }
+    });
+    _stringifier.on('finish', function(){
+        let output_string = _output_data.join();
+        fs.writeFile(_output_path, output_string);
+    })
+    const parser = parse({
+        delimiter: ','
+    });
+    _parsed_lines = [];
+    parser.on('readable', function() {
+        let line
+        while(line = parser.read()) {
+            console.log(line);
+            _parsed_lines.push(line);
+        }
+    });
+    parser.on('end', function() {
+        _stringifier.write(_parsed_lines.shift());
+        test_line();
+    });
+    fs.readFile(_csv_path, 'utf8', function(err, data) {
+        parser.write(data);
+        parser.end();
+    });
+}
+
+function test_line() {
+    if (_parsed_lines.length == 0) {
+        console.log("parsed lines empty!!!!");
+        _stringifier.end();
+        return;
+    }
+    let line = _parsed_lines.shift();
+    console.log(line[0]);
+    _search_str = line[0].trim();
+    _desired_id = line[2];
+    _cur_line = line;
+    _next_actions = [0, 0, 0];
+    refresh_browse({ pop_all: true });
+}
+
+function check_result(items) {
+    console.log("check result!!!");
+    console.log(items);
+    let subtitle = items["subtitle"];
+    let ids = subtitle.split("|");
+    var found = ids.filter(x => _desired_id.split("|").includes(x)).length;
+    //var found = ids.includes(_desired_id) ? 1 : 0;
+    var out_line = _cur_line.concat([found]);
+    console.log(out_line);
+    _stringifier.write(out_line);
+    test_line();
+}
+
+function refresh_browse(opts) {
+    opts = Object.assign({
+        hierarchy:          "browse",
+        zone_or_output_id:  _output_id,
+    }, opts);
+
+    _browse.browse(opts, (err, r) => {
+        if (err) { console.log(err, r); return; }
+
+        console.log(err, r);
+
+        if (r.action == 'list') {
+            _view["list"] = r.list;
+            _view["items"] = [];
+            var listoffset = r.list.display_offset > 0 ? r.list.display_offset : 0;
+            load_browse(listoffset);
+
+        } else if (r.action == 'message') {
+            alert((r.is_error ? "ERROR: " : "") + r.message);
+
+        } else if (r.action == 'replace_item') {
+            var i = 0;
+            var l = _view.items;
+            while (i < l.length) {
+                if (l[i].item_key == opts.item_key) {
+                    l.splice(i, 1, r.item);
+                    break;
+                }
+                i++;
+            }
+            _view["items"] = l;
+
+        } else if (r.action == 'remove_item') {
+            var i = 0;
+            var l = _view.items;
+            while (i < l.length) {
+                if (l[i].item_key == opts.item_key) {
+                    l.splice(i, 1);
+                    break;
+                }
+                i++;
+            }
+            _view["items"] = l;
+
+        } else if (r.action == 'none') {
+            check_result(r.list);
+        } else {
+            console.log("unexpected result from browse, aborting!!!");
+        }
+    });
+}
+
+function load_browse(listoffset) {
+    _browse.load({
+        hierarchy:          "browse",
+        offset:             listoffset,
+        set_display_offset: listoffset,
+    }, (err, r) => {
+        _view["listoffset"] = listoffset;
+        _view["items"] = r.items;
+        console.log("load_browse, length: " + _next_actions.length);
+        if (_next_actions.length != 0) {
+            let item = _view.items[_next_actions.shift()];
+            var opts = { item_key: item.item_key };
+            if (item.input_prompt != undefined) {
+                opts.input = _search_str;
+            }
+            refresh_browse(opts);
+        } else {
+            check_result(r.list);
+        }
+    });
+}
+
 roon.init_services({
-    required_services:   [ RoonApiTransport ],
+    required_services:   [ RoonApiBrowse ],
     provided_services:   [ svc_settings ]
 });
 
